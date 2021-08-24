13dda5adf9985e9e5912cc13e60796dcc9eb9445
Nguyen Huu Nhan <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2021-08-24 14:31:04 +0700

    Library

---
+++ Library/Stack_LTDT.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define idx 100
+typedef struct{
+    int data[idx];
+    int size;
+}Stack;
+void makenull_Stack(Stack *s){
+    s->size=0;
+}
+bool empty_Stack(Stack *s){
+    return s->size==0;
+}
+void push_Stack(Stack *s, int element){
+    s->data[s->size]=element;
+    s->size++;
+}
+int get_Stack(Stack *s){
+    return s->data[s->size-1];
+}
+void delete_size(Stack *s){
+    s->size--;
+}
