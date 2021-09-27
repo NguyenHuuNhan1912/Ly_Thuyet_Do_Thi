f7cb0abce75ddd4ffc917fad1c2332980f52dba5
Nguyen Huu Nhan <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2021-09-27 12:42:57 +0700

    Tôn ngộ không

--- Thuc_Hanh/Buoi_2/BT_4.c
+++ Thuc_Hanh/Buoi_2/BT_4.c
@@ -1,8 +1,7 @@
 /*
+
 Tôn Ngộ Không là một trong các nhân vật chính của truyện Tây du ký. Khi còn ở Hoa Quả Sơn, Tôn Ngộ Không là vua của loài khỉ. Hoa Quả Sơn có rất nhiều cây ăn trái, nên loài khỉ rất thích. Do đặc tính của mình, khỉ không thích đi bộ mà chỉ thích chuyền từ cây này sang một cây khác. Tuy nhiên, nếu khoảng cách giữa hai cây quá xa thi chúng không thể chuyền qua lại được.
-
 Đường đường là vua của loài khỉ, Tôn Ngộ Không muốn vạch ra một kế hoạch hái trái cây trên tất cả các cây có trên Hoa Quả Sơn mà không cần phải nhảy xuống đất. Tôn Ngộ Không dự định sẽ bắt đầu leo lên một cây, hái trái của cây này, sau đó chuyền qua một cây kế tiếp hái trái của này và tiếp tục như thế cho đến khi tất cả các cây đều được hái trái. Một cây có thể được chuyền qua chuyền lại nhiều lần.
-
 Hãy giúp Tôn Ngộ Không kiểm tra xem kế hoạch này có thể thực hiện được không.
 
 Đầu vào (Input):
@@ -19,41 +18,64 @@ Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
 Nếu kế hoạch của Tôn Ngộ Không có thể thực hiện được DUOC, ngược lại in ra KHONG.
 
 Xem thêm ví dụ bên dưới. Trong ví dụ đầu tiên, Tôn Ngộ Không bắt đầu từ cây 1, chuyền qua cây 2, sau đó chuyền ngược về 1, chuyền tiếp sang 3 và sau cùng là sang 4.
+
+For example:
+
+Input	Result
+
+4 3     DUOC 
+2 1
+1 3
+3 4
+
+4 2     KHONG    
+1 2
+3 4
+
 */
 
 //code
-
-//Ton ngo khong - Ly thuyet do thi
 
 #include <stdio.h>
 #include <stdbool.h>
 #define maxv 50
 #define idx 100
 typedef int Vertices;
-int n,m;
+
+//Khai bao cau truc do thi
 typedef struct{
     int matrix[maxv][maxv];
-    Vertices n;//n dinh
+    Vertices n;//Dinh n
 }Graph;
-//Cau truc list
+
+int n,m;//Bien toan cuc luu so dinh va so canh
+
+//Khai bao cau truc danh sach - LIST
 typedef struct{
     int data[idx];
     int size;
 }List;
-//makenullLsit
+
+/*Cac ham tren danh sach*/
+
+//Lam rong danh sach
 void makenullList(List *l){
     l->size=0;
 }
-//Push
-void pushList(List *l, int element){
-    l->data[l->size]=element;
+
+//Them phan tu vao danh sach
+void pushList(List *l, int x){
+    l->data[l->size]=x;
     l->size++;
 }
-//get
-int getList(List *l, int index){
-    return l->data[index];
+
+//Truy cap phan tu trong danh sach
+int getList(List *l, int x){
+    return l->data[x];
 }
-//Khoi tao
+/*Cac ham tren do thi*/
+
+//Khoi tao do thi rong
 void initGraph(Graph *g){
     g->n=n;
     for(int i=1;i<=n;i++){
@@ -62,55 +84,67 @@ void initGraph(Graph *g){
         }
     }
 }
-//ADD
+
+//Them cung e = (x,y) vao do thi
 void addGraph(Graph *g, int x, int y){
     g->matrix[x][y]=1;
     g->matrix[y][x]=1;
 }
-//Nei
-List neighbors(Graph *g, int x){
+
+//Tim dinh lang gieng cua dinh x (x la dinh dinh bat ki trong do thi)
+List neighbors(Graph g, int x){
     List L;
     makenullList(&L);
     for(int i=1;i<=n;i++){
-        if(g->matrix[x][i]==1){
+        if(g.matrix[x][i]==1){
             pushList(&L,i);
         }
     }
     return L;
 }
-//Duyet do thi de qui
-int mark[maxv];
-int count;
-void DFS_Discursion(Graph *g, int x){
-    if(mark[x]==1) return;
-    //printf("%d\n",x);
-    count++;
-    mark[x]=1;
-    List L = neighbors(g,x);
+
+int count;//Bien toan cuc kiem tra do thi co lien thong hay khong
+int mark[maxv];//Mang toan cuc danh dau 1 dinh da duyet vao do thi hay chua
+
+//Duyet do thi theo chieu sau bang de qui
+void DFS_Recursion(Graph *g, int x){
+    if(mark[x]==1) return; //Neu dinh do da duyet thi kh duyet nua 
+    mark[x]=1; //Danh dau dinh do da duyet
+    count++; //Tang bien count len 1 don vi
+    List L = neighbors(*g,x); //Tim dinh lang gieng cua dinh x luu vao danh sach
     for(int j=0;j<L.size;j++){
-        int v = L.data[j];
-        DFS_Discursion(g,v);
+        int v = getList(&L,j); //Lay dinh ke dau tien cua dinh x trong danh sach ra
+        DFS_Recursion(g,v); //Goi de qui
     }
+    
 }
-bool check(Graph *g){
-    DFS_Discursion(g,1);
-    if(n==count) return true;
-    return false;
+
+//Kiem tra
+bool check(){
+    //Neu bien count = so dinh cua do thi --> Do thi vo huong lien thong
+    //Do thi duoc goi la lien thong chi ton tai duy nhat 1 thanh phan lien thong
+    return (n==count);
 }
 int main(){
+
     Graph g;
-    count=0;
+    int u,v;
     scanf("%d%d",&n,&m);
     initGraph(&g);
-    int u,v;
     for(int i=1;i<=m;i++){
         scanf("%d%d",&u,&v);
         addGraph(&g,u,v);
     }
-    for(int i=1;i<=n;i++){
-        mark[i]=0;
-    }
-    if(check(&g)) printf("DUOC");
+
+    //Khoi tao cac phan tu trong mark ban dau = 0
+    for(int i=1;i<=n;i++) mark[i]=0;
+
+    //Goi ham de qui bat dau tu dinh 1
+    DFS_Recursion(&g,1);
+
+    //Goi ham de kiem tra
+    if(check()) printf("DUOC");
     else printf("KHONG");
+
     return 0;
 }
