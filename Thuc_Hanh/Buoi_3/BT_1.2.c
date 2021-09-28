4d6d2a09bd5e7e9c0de13c06fdbc6b1192ea13a2
Nguyen Huu Nhan <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2021-09-28 08:31:33 +0700

    Tìm đường đi ngắn nhất - đơn đồ thị vô hướng

--- Thuc_Hanh/Buoi_3/BT_1.2.c
+++ Thuc_Hanh/Buoi_3/BT_1.2.c
@@ -1,3 +1,63 @@
+/*
+Cho đồ thị vô hướng G = <V, E> có n đỉnh và m cung (n < 100, m < 500). Mỗi cung được gán một trọng số w (0 < w <= 100).
+
+Viết chương trình tìm đường đi ngắn nhất từ đỉnh 1 đến n.
+
+Đầu vào (Input):
+
+Dữ liệu đầu vào được nhập từ bàn phím với định dạng:
+
+- Dòng đầu tiên chứa 2 số nguyên n và m.
+
+- m dòng tiếp theo mỗi dòng chứa 3 số nguyên u, v, w mô tả cung (u, v) có trọng số w.
+
+Đầu ra (Output):
+
+In ra màn hình chiều dài của đường đi ngắn nhất từ 1 đến n. Nếu không có đường đi từ 1 đến n, in ra -1.
+
+Xem thêm ví dụ bên dưới.
+
+Chú ý:
+
+Để chạy thử chương trình, bạn nên tạo một tập tin dt.txt chứa đồ thị cần kiểm tra.
+Thêm dòng freopen("dt.txt", "r", stdin); vào ngay sau hàm main(). Khi nộp bài, nhớ gỡ bỏ dòng này ra.
+Có thể sử dụng đoạn chương trình đọc dữ liệu mẫu sau đây:
+	freopen("dt.txt", "r", stdin); //Khi nộp bài nhớ bỏ dòng này.
+	Graph G;
+	int n, m, u, v, w, e;
+	scanf("%d%d", &n, &m);
+	init_graph(&G, n);
+	
+	for (e = 0; e < m; e++) {
+		scanf("%d%d%d", &u, &v, &w);
+		add_edge(&G, u, v, w);
+	}
+
+For example:
+
+Input	Result
+3 3     4  
+1 2 9
+2 3 4
+1 3 4
+
+3 1    -1
+1 2 5
+
+6 9    20
+1 2 7
+1 3 9
+1 5 14
+2 3 10
+2 4 15
+3 4 11
+3 5 2
+4 6 6
+5 6 9
+*/
+
+//code
+
 #include <stdio.h>
 #include <stdbool.h>
 #define maxv 50 
@@ -90,7 +150,6 @@ int main(){
 		scanf("%d%d%d",&u,&v,&t);
 		addEdge(&g,u,v,t);
 	}
-	
 	Dijkstra(&g,1);
 	
 	if(pi[n]==INFINITY) printf("-1");
