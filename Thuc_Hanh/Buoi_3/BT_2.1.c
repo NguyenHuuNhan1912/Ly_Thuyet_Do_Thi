d3fb1721ce5e65ea0e0884e748eeb44619150ac1
Nguyen Huu Nhan <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2021-10-02 10:27:33 +0700

    Kiểm tra chu trình âm - đơn đồ thị có hướng

--- Thuc_Hanh/Buoi_3/BT_2.1.c
+++ Thuc_Hanh/Buoi_3/BT_2.1.c
@@ -113,58 +113,6 @@ void addEdge(Graph *g, int x, int y, int t){
 	g->matrix[x][y]=t;
 }
 
-//Hàm tìm giá trị nhỏ nhất
-int min(int a, int b){
-    return (a<b) ? a: b;
-}
-
-int mark[maxv]; //Mảng toàn cục để đánh dấu các đỉnh
-int pi[maxv]; //Mảng toàn cục để lưu đường đi ngắn nhất tạm thời
-int p[maxv]; //Mảng toàn cục để lưu đỉnh cha của một đỉnh
-
-//Thuật toán tìm đường đi ngắn nhất
-void Dijkstra(Graph *g, int x){
-
-	int i,j,k;
-	for(i=1;i<=n;i++){
-		pi[i] = INFINITY; //Khởi tạo đường đi ngắn nhất tạm thời của tất cả các đỉnh bằng vô cực
-		mark[i]=0; //Khởi tạo chưa có đỉnh nào được đánh dấu( Đỉnh chưa đánh dâu có chỉ số bằng 0)
-	}
-
-	pi[x] = 0;//Xét đỉnh đầu tiên nên ta thay đổi pi của đỉnh đầu tiên từ vô cực sang 0 để bắt đầu thuật toán
-	p[x] = -1;//Đỉnh cha của đỉnh đầu tiên gán bằng -1 tức đỉnh 1 không có đỉnh cha
-
-	//Tìm đỉnh chưa đánh dấu(mark[x]==0) và có pi[x] nhỏ nhất để xét
-	for(k=1;k<=n;k++){
-		int min_pi = INFINITY;
-		for(j=1;j<=n;j++){
-			if(mark[j] == 0 && pi[j] < min_pi){
-				min_pi = pi[j];
-				i=j;
-			}
-		}
-
-		mark[i]=1;//Sau khi đã tìm được đỉnh có pi[index] nhỏ nhất thì đánh dấu đỉnh đó đã được duyệt
-		
-		for(j=1;j<=n;j++){
-			int v = g->matrix[i][j];
-			//Nếu v là đỉnh kề của đỉnh x (tức đường đi từ x tới v có trọng số khác 0/NO_EDGE) và mark[v] chưa được duyệt
-			if( v !=  NO_EDGE  && mark[j]==0){ 
-			    int pi_before = pi[j];//pi_before để lưu đường đi ngắn nhất tạm thời trước đó
-				int pi_after = min(pi[j],pi[i]) + g->matrix[i][j]; //pi_after là đường đi ngắn nhất tạm thời vừa mới tính được
-				//Nếu đường đi ngắn nhất tạm thời sau < đường đi ngắn nhất tạm thời trước đó
-				if(pi_after < pi_before ){
-				    pi[j]=pi_after;//Cập nhật lại đường đi ngắn nhất tạm thời = đường đi ngắn nhất tạm thời vừa tìm được
-				    p[j]=i;//Sau khi cập nhật xong thì lưu đỉnh cha của đỉnh vừa cập nhật 
-				}
-				/*Nếu câu điều kiện phía trên không thực hiện thì đường đi ngắn nhất tạm thời sau không 
-				lớn hơn đường đi ngắn nhất tạm thời trước đó thì không cập nhật mà chỉ lưu đỉnh cha của đỉnh đó*/
-				p[j]=i;    
-			}
-		}
-	}
-}
-
 //Hàm kiểm tra
 int check(int ts[]){
     int total=0;
@@ -174,17 +122,17 @@ int check(int ts[]){
     return total;
 }
 int main(){
-	Graph g;
+    Graph g;
     scanf("%d%d",&n,&m);
     int ts[m];//Mảng lưu các trọng số của các cung trong đồ thị
-	initGraph(&g);
-	int u,v,t;
-	for(int i=1;i<=m;i++){
-		scanf("%d%d%d",&u,&v,&t);
-		ts[i] = t;
-		addEdge(&g,u,v,t);
-	}
-	Dijkstra(&g,1);
+    initGraph(&g);
+    int u,v,t;
+    for(int i=1;i<=m;i++){
+          scanf("%d%d%d",&u,&v,&t);
+	  ts[i] = t;
+          addEdge(&g,u,v,t);
+    }
+    Dijkstra(&g,1);
     if(check(ts)<0) printf("negative cycle"); //Đồ thị có chứa chu trình âm
     else printf("ok"); //Đồ thị không chứa chu trình âm
 	return 0;
