b488e1c607a6f839117844dc9d05ebdacb174b84
Nguyen Huu Nhan <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2021-10-13 20:00:03 +0700

    Xếp hạng đồ thị

--- Thuc_Hanh/Buoi_4/BT_1.c
+++ Thuc_Hanh/Buoi_4/BT_1.c
@@ -76,128 +76,148 @@ Input	Result
 
 //code
 
+// Xếp hạng đồ thị
 #include <stdio.h>
+#include <stdbool.h>
 #define maxv 50
 #define idx 100
-int n,m;//Biến toàn cục lưu số đỉnh và số cung
-typedef int Vertices;
-//Khai báo cấu trúc đồ thị
-typedef struct {
-    int matrix[maxv][maxv];
-    Vertices n; //Đỉnh n
-}Graph;  
+int n,m; //Biến toàn cục lưu số đỉnh và số cung
+typedef int Vertices; //Đỉnh n trong đồ thị
 
-//Khai báo cấu trúc danh sách
-typedef struct {
+//Khai báo cấu trúc đồ thị - GRAPH
+typedef struct{
+    int matrix[maxv][maxv];
+    Vertices n;//Đỉnh n
+}Graph;
+
+//Khai báo cấu trúc danh sách - LIST
+typedef struct{
     int data[idx];
     int size;
-} List;
+}List;
+/* Các hàm trên danh sách */
 
-//Khởi tạo danh sách rỗng
-void makenullList(List *l) {
+//Làm rỗng danh sách
+void makenullList(List *l){
     l->size=0;
 }
 
 //Thêm một phần tử vào danh sách
-void pushList(List *l, int x) {
-    l->data[l->size] = x;
+void pushList(List *l, int element){
+    l->data[l->size] = element;
     l->size++;
 }
-
-//Lấy phần tử trong danh sách bắt đầu vị trí thứ 1
-int getList(List *l, int i) {
-    return l->data[i-1];
+//Truy cập phần tử trong danh sách bắt đầu tử vị trí thứ 1 
+int getList(List *l, int index){
+    return l->data[index-1];
 }
 
-//Khởi tạo đồ thị
+//Kiểm tra danh sách có rỗng hay không
+bool emptyList(List *l){
+    return l->size==0;
+}
+
+/* Các hàm trên đồ thị */
+
+//Khởi tạo
 void initGraph(Graph *g){
-    g->n = n;
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=n;j++)
+    g->n=n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
             g->matrix[i][j]=0;
+        }
+    }
 }
 
-//Thêm cung e = (x,y) vào đồ thị
-void addEdge(Graph *g, int x,int y){
+//Thêm cung e = (x,y) vào đồ thị có hướng
+void addEdge(Graph *g, int x, int y){
     g->matrix[x][y]=1;
 }
 
-//Tính bậc vào của 1 đỉnh
+//Kiểm tra 2 đỉnh có kề nhau hay không
+bool adjacent(Graph *g, int x, int y){
+    return (g->matrix[x][y]==1);
+}
+
+//Tính bậc vào của một đỉnh
 int degreeInput(Graph *g, int x){
-	int count=0;
-	for(int i=1;i<=n;i++){
-		if(g->matrix[i][x]==1){
-			count++;
-		}
-	}
-	return count;
-}
-
-//Hàm copy danh sách
-void copyList(List *s1, List *s2){
-    int i, x;
-    makenullList(s1);
-    for(i=1;i<=s2->size;i++){
-        x=getList(s2,i);
-        pushList(s1,x);
-    }
-}
-//Các biến hỗ trợ thuật toán xếp hạng đồ thị 
-int rank[maxv]; //Mảng toàn cục lưu hạng của một đỉnh
-int d[maxv]; //Mảng toàn cục lưu bậc của một đỉnh
-int k=0; // Bước lặpkk
-List S1, S2; // Danh sách s1 lưu các đỉnh gốc cũ, danh sách s2 lưu các đỉnh gốc mới
-
-//Thuật toán xếp hạng đồ thị
-void ranking(Graph *g){
-
-    int u;
-    for(u=1;u<=n;u++){
-        d[u] = 0; //Khởi tạo bậc của tất cả các đỉnh ban đầu bằng 0
-    }
-	
-	//Tính bậc vào của tất cả các đỉnh trong đồ thị 
-	for(u=1;u<=n;u++){
-		d[u] = degreeInput(g,u);
-	}
-    
-    makenullList(&S1);//Khởi tạo danh sách chứa các đỉnh gốc ở bước lặp k
-
-    for(u=1;u<=n;u++){
-	if(d[u] == 0){ //Chọn đỉnh gốc có bậc vào bằng 0 vừa tính ở trên
-		pushList(&S1, u); //Đưa đỉnh đó vào danh sách s1
-	}
-    }   
-
-    int i;
-    while(S1.size > 0){ //Lặp trong khi S1 chưa rỗng
-        makenullList(&S2); //Khởi tạo danh sách S2 để lưu các đỉnh v kề u mà có bậc gốc = 0
-        for(i=1;i<=S1.size;i++){
-            int u = getList(&S1,i); //Lấy đỉnh u trong S1 ra
-            rank[u] = k; //Tính hạng của đỉnh u = bước lặp k
-            for (int v=1;v<=n;v++){//Tìm các đỉnh v kề u
-				if(g->matrix[u][v] == 1){ //Nếu v là đỉnh kề của u
-                    d[v]--; //Giảm bậc của v đi việc này tương đương như xóa gốc u ra khỏi đồ thị
-                	if(d[v] == 0) //Sau khi giảm bậc nếu v là bậc gốc thì đưa v vào S2
-                    	pushList(&S2, v);//Đưa v vào s2
-                }
-			} 
+    int count=0;
+    for(int i=1;i<=n;i++){
+        if(g->matrix[i][x]==1){
+            count++;
         }
-        copyList(&S1, &S2); //Lấy các đỉnh trong s2 đưa vào s1 để tiếp tục xét
-        k++;//Tăng bước lặp k để tiếp tục thuật toán
+    }
+    return count;
+}
+//Hàm sao chép phần tử từ danh sách này sang danh sách khác
+void copyList(List *l1, List *l2){
+    makenullList(l1);
+    for(int i=1;i<=l2->size;i++){
+        int value = getList(l2,i);
+        pushList(l1,value);
     }
 }
-int main (){
+
+//Các biến toàn cục để phục vụ giải thuật xếp hạng
+int rank[maxv];//Mảng toàn cục lưu hạng của từng đỉnh 
+int d[maxv];//Mảng toàn cục lưu bậc vào của từng đỉnh
+int k=0; //Bước lặp của giải thuật
+List L1,L2;//Danh sách L1 lưu đỉnh có bậc vào bằng 0(gốc cũ), danh sách L2 lưu những đỉnh kề v của u có bậc vào bằng 0(gốc mới)
+
+//Giải thuật xếp hạng đô thị
+void ranking(Graph *g){
+    int u;
+    //Khởi tạo bậc vào của các đỉnh đều bằng 0
+    for(u=1;u<=n;u++){
+        d[u] = 0;
+    }
+
+    //Tính bậc vào của các đỉnh
+    for(u=1;u<=n;u++){
+        d[u] = degreeInput(g,u);
+    }
+
+    makenullList(&L1); //Khởi tạo danh sách L1 chứa các đỉnh gốc(đỉnh gốc là đỉnh có bậc vào bằng 0)
+    for(u=1;u<=n;u++){
+        if(d[u]==0){
+            pushList(&L1,u); //Các đỉnh gốc sẽ được đưa vào L1
+        }
+    }
+
+    while(!emptyList(&L1)){ //Lặp trong khi L1 chưa rỗng
+        makenullList(&L2); //Khởi tạo danh sách L2 để chứa các đỉnh kề v của u có bậc vào bằng 0
+        for(int i=1;i<=L1.size;i++){
+            int u = getList(&L1,i); //Lấy đỉnh gốc trong danh sách L1 ra
+            rank[u] = k; //Xếp hạng cho đỉnh đó bằng bước lặp k
+            for(int v=1;v<=n;v++){ //Tìm đỉnh kề của đỉnh vừa được xếp hạng
+                if(adjacent(g,u,v)){ //Nếu kề
+                    d[v]--; //Giảm bậc của đỉnh kề tương đương với việc xóa đỉnh gốc
+                    if(d[v]==0){ //Nếu đỉnh kề có bậc bằng 0 -> Đây là đỉnh gốc mới
+                        pushList(&L2,v);//Đưa đỉnh gốc mới vào L2
+                    }
+                }
+            }
+        }
+        copyList(&L1,&L2); //Di chuyển các đỉnh gốc mới trong L2 sang L1
+        k++; //Tăng bước lặp k lên để tiếp tục xếp hạng cho các đỉnh còn lại
+        //Lặp đến khi danh sách L1 rỗng tức không còn đỉnh kề nào -> Giải thuật kết thúc
+    }
+}
+int main(){
     Graph g;
-    int u, v, e;
-    scanf("%d%d", &n, &m);
+    int u,v;
+    scanf("%d%d",&n,&m);
     initGraph(&g);
-    for (e = 0; e < m; e++) {
-        scanf("%d%d", &u, &v);
-        addEdge(&g, u, v);
+    for(int e=1;e<=m;e++){
+        scanf("%d%d",&u,&v);
+        addEdge(&g,u,v);
     }
     ranking(&g);
-    for(u=1;u<=n;u++)
-        printf("%d \n",rank[u]);
+    for(int i=1;i<=n;i++)
+        printf("%d\n",rank[i]);
     return 0;
 }
+/*
+NOTE: Khi áp dụng giải thuật xếp hạng vào các bài toán thực tế thì bước
+lặp k có thể bắt đầu từ 1 chứ không nhất thiết phải bắt đầu bằng 0
+*/
